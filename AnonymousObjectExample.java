c5fa95612abdee3d617515d674b5fcd9c0c94cc9
Abid Aziz <azizabid240@gmail.com>
2024-07-01 20:11:39 +0600

    A

---
+++ AnonymousObjectExample.java
@@ -0,0 +1,12 @@
+public class AnonymousObjectExample {
+    void display() {
+        System.out.println("Anonymous Object Example");
+    }
+
+    public static void main(String[] args) {
+        new AnonymousObjectExample().display();  // Output: Anonymous Object Example
+    }
+}
+/*
+ * An anonymous object is an object without a reference variable. It is used only once and is generally used for instantiating classes and invoking methods on the fly.
+ */
